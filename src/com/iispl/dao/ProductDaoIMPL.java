c46f584c190f973a84d7f8f77d7c218bb802d354
Ankmmarao <ankammaraomanimela70@gmail.com>
2026-07-29 12:10:13 +0530

    Fixed product insert query and database connection

--- src/com/iispl/dao/ProductDaoIMPL.java
+++ src/com/iispl/dao/ProductDaoIMPL.java
@@ -2,11 +2,6 @@ package com.iispl.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.sql.DataSource;
 
 import com.iispl.connectionpool.ConnectionPool;
@@ -14,37 +9,37 @@ import com.iispl.entity.Product;
 
 public class ProductDaoIMPL implements ProductDao {
 
+    @Override
+    public void createProduct(Product product) {
 
-	@Override
-	public void createProduct(Product product) {
-	
-		// TODO Auto-generated method stub
-        String selectQuery = "INSERT INTO Product(productcode,product_name,product_description,activation_date,expiry_date) values(?,?,?,?,?)";
+        String insertQuery = 
+            "INSERT INTO \"Product\" " +
+            "(productcode, productName, productdescription, activationDate, expiryDate) " +
+            "VALUES (?, ?, ?, ?, ?)";
 
-		try {
-			DataSource ds = ConnectionPool.getDataSource();
-			Connection connection = ds.getConnection();
-			PreparedStatement pstmt = connection.prepareStatement(selectQuery);
-			pstmt.setString(1, product.getProductcode());
-			pstmt.setString(2, product.getProductName());
-			pstmt.setString(3, product.getProductdescription());
-			pstmt.setObject(4, product.getActivationDate());
-			pstmt.setObject(5, product.getExpiryDate());
-			int result=pstmt.executeUpdate();
-			if(result>0) {
-				System.out.println("Product Addesd SUccesfully");
-			}
-			
-		}
-		catch(Exception e) {
-			
-		}
-		
-	}
+        try {
+            DataSource ds = ConnectionPool.getDataSource();
 
-	
-	
-		
+            try (Connection connection = ds.getConnection();
+                 PreparedStatement pstmt = connection.prepareStatement(insertQuery)) {
 
-	
+                pstmt.setString(1, product.getProductcode());
+                pstmt.setString(2, product.getProductName());
+                pstmt.setString(3, product.getProductdescription());
+                pstmt.setObject(4, product.getActivationDate());
+                pstmt.setObject(5, product.getExpiryDate());
+
+                int result = pstmt.executeUpdate();
+
+                if (result > 0) {
+                    System.out.println("Product Added Successfully");
+                } else {
+                    System.out.println("Product Not Added");
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
